8f6e58f98d8724d3e4d584ec4fbd971d14bf97c1
Lewis <marshy2201@yahoo.co.uk>
2020-10-08 12:22:06 +0100

    Updates

---
+++ precache-manifest.4cd14da3c24dffe5aad042958bfbbfe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a13010240a4f121e9d262ef166ec3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af72ce08c56ef0b77b8f",
+    "url": "/static/css/main.be4cde2f.chunk.css"
+  },
+  {
+    "revision": "a7fd294de0c266736989",
+    "url": "/static/js/2.ec25e640.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec25e640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af72ce08c56ef0b77b8f",
+    "url": "/static/js/main.4714f635.chunk.js"
+  },
+  {
+    "revision": "5a20132809060f46f828",
+    "url": "/static/js/runtime-main.51dfbac9.js"
+  }
+]);
